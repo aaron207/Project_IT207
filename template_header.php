83182dbfb5b1eff181e5f065fb418dc0e1129909
aaron207 <abeckx86@gmail.com>
2014-06-14 22:52:56 -0400

    Create template_header.php

---
+++ template_header.php
@@ -0,0 +1,24 @@
+
+<head>
+<style type="text/css">
+.auto-style1 {
+	text-align: center;
+}
+</style>
+</head>
+
+<div id="pageHeader"><table width="100%" border="0" cellspacing="0" cellpadding="12">
+  <tr>
+    <td style="width: 28%"><a href="index.php">
+	<img src="ball.jpg" alt="Logo" width="101" height="105" border="0" /></a> 
+	Soccer World</td>
+    <td width="68%" align="right"></td>
+  </tr>
+  <tr>
+    <td colspan="2" class="auto-style1"><a href="index.php">Home</a>	<a href="products.php">Products</a>	<a href="customer_reviews.php">Customer Reviews</a>	<a href="contact_us.php">Contact Us</a></td>
+    </tr>
+  <tr>
+    <td colspan="2" class="auto-style1">&nbsp;</td>
+    </tr>
+  </table>
+</div>
