5106664eb86c5ac819f8404f4f597bc835998486
aaron207 <user@Craigs-MacBook-Pro.local>
2014-06-15 21:07:33 -0400

    contact_us.php Text and title

--- contact_us.php
+++ contact_us.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Store Admin Area</title>
+<title>Contact Us</title>
 
 <link href="style.css" rel="stylesheet" type="text/css" />
 
@@ -15,10 +15,15 @@
     <div align="left" style="margin-left:24px;">
       <h2>Contact Us</h2>
       <hr>
-    </div>
-    	<br />
+    
+    	<p> We are a student owned business with a passion for soccer. Feel free to contact us with suggestions for how to improve the site or an issues you have while browsing! <br /> <br />
+    	
+    	<b>Phone</b> 703-993-9999 <br />
+    	<b>Suggestions</b> suggestions@mason.edu <br />
+    	<b>Complaints</b> complaints@mason.edu <br /> 
   		<br />
 		<br />
+    	</align>
 	</div>
 <?php include_once("template_footer.php");?>
 </div>
